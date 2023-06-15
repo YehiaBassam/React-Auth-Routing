69b19e4616cc40eb9ff113ae81be6cd7ae35200f
Yahia bassam <ybassam@2pegypt.local>
2023-06-15 16:02:20 +0300

    upadte | add Error page

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import { useRouteError } from 'react-router-dom';
 import MainNavigation from '../MainNavigation/MainNavigation';
 
-const Error = () => {
+function Error() {
+  const error = useRouteError();
+
+  let title = 'An error occurred!';
+  let message = 'Something went wrong!';
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
+  }
+
   return (
     <>
       <MainNavigation />
-      <main>
-        <h1>An error occurred!</h1>
-        <p>Could not find this page!</p>
-      </main>
+      <div style={{textAlign: 'center'}}>
+        <h2>{title}</h2>
+        <p>{message}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Error
+export default Error;
